772f04bfc58d709007f3e1f219d74b967a8bf62e
Johan Ariel <maphuders4@gmail.com>
2024-03-06 20:02:48 +0700

    lobbyUpdate

--- Assets/Scripts/Photon Scripts/ConnectToServer.cs
+++ Assets/Scripts/Photon Scripts/ConnectToServer.cs
@@ -2,22 +2,25 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class ConnectToServer : MonoBehaviourPunCallbacks
 {
-    // Start is called before the first frame update
-    private void Start()
+    public InputField usernameInput;
+    public Text buttonText;
+
+    public void OnClickConnect()
     {
-        PhotonNetwork.ConnectUsingSettings();
+        if(usernameInput.text.Length >= 1)
+        {
+            PhotonNetwork.NickName = usernameInput.text;
+            buttonText.text = "Connecting...";
+            PhotonNetwork.ConnectUsingSettings();
+        }
     }
 
     public override void OnConnectedToMaster()
-    {
-        PhotonNetwork.JoinLobby();
-    }
-
-    public override void OnJoinedLobby()
     {
         SceneManager.LoadScene("Lobby");
     }
